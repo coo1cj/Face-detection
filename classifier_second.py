f830a494febb1460bcddcc657a1cb3464e295186
Coo1cj <56195074+coo1cj@users.noreply.github.com>
2020-04-24 23:28:31 +0200

    choose the best parameters

--- classifier_second.py
+++ classifier_second.py
@@ -8,16 +8,14 @@ import random as rand
 import joblib
 
 
-### load the trained classifier before
-clf = joblib.load('final_classifier/last_train_model_1.m')
-
 ### load the truepositive and falsepositive samples
 fp = np.loadtxt('falsepositive1.txt',dtype=int)
-tp = np.loadtxt('truepositive.txt',dtype=int)
+ros = np.loadtxt('ros.txt',dtype=int)
 fp1 = np.loadtxt('falsepositive2.txt',dtype=int)
 
-x = np.zeros((14023,3888))
-y = np.hstack((np.zeros(12484),np.ones(1539)))
+
+x = np.zeros((15307,3888))
+y = np.hstack((np.zeros(12484),np.ones(2823)))
 
 ### add the negative samples
 count = 0
@@ -57,17 +55,22 @@ for i in range(1,1001):
         count1 += 1
 
 
+
 ### add the positive samples
 for i in range(1284):
     I = io.imread('train_pos/%04d.jpg'%i, as_gray=True)
     I = cv2.resize(I,(64,64))
     x[i+12484,:] = hog(I,orientations=12,transform_sqrt=True)
 
+for i in range(1284):
+    I = io.imread('apresrotation/%04d.jpg'%i, as_gray=True)
+    I = cv2.resize(I,(64,64))
+    x[i+13768,:] = hog(I,orientations=12,transform_sqrt=True)   
 
 c1 = 0
 for i in range(1,1001):
     I = io.imread('train/%04d.jpg'%i,as_gray=True)
-    a = tp[tp[:,0] == i]
+    a = ros[ros[:,0] == i]
 
     for j in range(len(a)):
         yy = a[j,1]
@@ -76,20 +79,39 @@ for i in range(1,1001):
         ww = a[j,4]
         f = I[yy:yy+hh,xx:xx+ww]
         f = cv2.resize(f,(64,64))
-        x[c1+13768,:] = hog(f,orientations=12,transform_sqrt=True)
+        x[c1+15052,:] = hog(f,orientations=12,transform_sqrt=True)
         c1 += 1
+
 
 
 ### cross-validation
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.25, random_state = rand.randint(1, 100))
 
-### see the result
-clf1 = clf.fit(x_train,y_train)
-print(clf1.score(x_test,y_test))
+### try the different paremeters
+""" C = np.linspace(0.01,3)
+dual = [True,False]
+losses=['hinge','squared_hinge']
+c_w = ['balanced',None]
+
+m = 0
+for c in C:
+    for l in losses:
+        for cw in c_w:
+            clf = LinearSVC(C=c,loss=l,class_weight=cw).fit(x_train,y_train)
+            if m < clf.score(x_test,y_test):
+                m = clf.score(x_test,y_test)
+                l_res = l
+                cw_res = cw
+                c_res = c
+print(m)
+print(l_res)
+print(cw_res)
+print(c_res) """
+
+
 
 ### training the classifier again
-clf1 = clf.fit(x,y)
-
+clf = LinearSVC(C=0.2,loss='hinge').fit(x,y)
 
 ### save new classifier
-joblib.dump(clf1,'final_classifier/last_train_model_2.m')
+joblib.dump(clf,'final_classifier/train_model_2.m')
